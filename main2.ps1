be95007ebc207322b40dbd710be325619fa0b268
Scripteur95 <mzamine@hotmail.fr>
2025-07-04 14:23:08 +0200

    Update main2.ps1

--- main2.ps1
+++ main2.ps1
@@ -1,10 +1,10 @@
 Write-Host "Bienvenue dans le script des TSSR0325"
 
 Write-Host "1 = Afficher les utilisateurs"
-Write-Host "2 = Création d'utilisateurs"
+Write-Host "2 = Creation d'utilisateurs"
 Write-Host "3 = Supprimer les utilisateurs"
 Write-Host "4 = Afficher les groupes"
-Write-Host "5 = Création de groupes"
+Write-Host "5 = Creation de groupes"
 Write-Host "6 = Supprimer les groupes"
 
 $choix = Read-Host "Entrez votre choix"
